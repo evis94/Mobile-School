8a8d7a6f2f8211055ed6a15da048154e44260d28
Eviatar Saidoff <esaidoff@chegg.com>
2017-09-16 22:05:52 +0300

    Added initial work

--- app/src/main/java/mobileschool/tables/MainActivity.java
+++ app/src/main/java/mobileschool/tables/MainActivity.java
@@ -1,13 +1,36 @@
 package mobileschool.tables;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.etSearch)
+    EditText etSearch;
+    @BindView(R.id.btnSearch)
+    Button btnSearch;
+    @BindView(R.id.tvResult)
+    TextView tvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        bindUI();
+    }
+
+    private void bindUI() {
+
+        btnSearch.setOnClickListener(view -> {
+            String text = etSearch.getText().toString();
+            tvResult.setText(text);
+        });
     }
 }
