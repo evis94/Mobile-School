6308071e5d75d0f7f7f1aabb6b4519b62aeab854
Eviatar Saidoff <esaidoff@chegg.com>
2017-09-16 22:37:14 +0300

    ListView no Viewholder pattern

--- app/src/main/java/mobileschool/tables/MainActivity.java
+++ app/src/main/java/mobileschool/tables/MainActivity.java
@@ -4,19 +4,27 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.Collections;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final int MAX_RESULTS = 70;
+
     @BindView(R.id.etSearch)
     EditText etSearch;
     @BindView(R.id.btnSearch)
     Button btnSearch;
-    @BindView(R.id.tvResult)
-    TextView tvResult;
+    @BindView(R.id.lvSearchResults)
+    ListView listViewResults;
+
+    SearchResultsAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +36,27 @@ public class MainActivity extends AppCompatActivity {
 
     private void bindUI() {
 
+
         btnSearch.setOnClickListener(view -> {
             String text = etSearch.getText().toString();
-            tvResult.setText(text);
+            List<String> results = Collections.nCopies(MAX_RESULTS, text);
+            showResultsInListView(results);
+
+        });
+    }
+
+    private void showResultsInListView(List<String> results) {
+
+        if (adapter == null) {
+            adapter = new SearchResultsAdapter(this, results);
+            listViewResults.setAdapter(adapter);
+        } else {
+            adapter.setSearchResults(results);
+            adapter.notifyDataSetChanged();
+        }
+
+        listViewResults.setOnItemClickListener((adapterView, itemView, pos, id) -> {
+            Toast.makeText(this, "List Item Clicked in pos: " + pos, Toast.LENGTH_SHORT).show();
         });
     }
 }
---
+++ app/src/main/java/mobileschool/tables/SearchResultsAdapter.java
@@ -0,0 +1,56 @@
+package mobileschool.tables;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by esaidoff on 16/09/2017.
+ */
+
+public class SearchResultsAdapter extends BaseAdapter {
+
+    List<String> results;
+    LayoutInflater layoutInflater;
+    private List<String> searchResults;
+
+    public SearchResultsAdapter(Context context, List<String> results) {
+
+        this.results = results;
+        layoutInflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public int getCount() {
+        return results.size();
+    }
+
+    @Override
+    public Object getItem(int pos) {
+        return results.get(pos);
+    }
+
+    @Override
+    public long getItemId(int pos) {
+        return pos;
+    }
+
+    @Override
+    public View getView(int pos, View view, ViewGroup viewGroup) {
+
+        View rowView = layoutInflater.inflate(R.layout.row_item, viewGroup, false);
+        TextView tvResult = rowView.findViewById(R.id.tvSearchResult);
+        String searchResult = results.get(pos);
+        tvResult.setText(searchResult + " " + pos);
+        return rowView;
+    }
+
+    public void setSearchResults(List<String> searchResults) {
+        this.searchResults = searchResults;
+    }
+}
