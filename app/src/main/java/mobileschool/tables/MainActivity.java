9a19c0c1bc203370d1a2ff4081f9317982c94b35
Eviatar Saidoff <esaidoff@chegg.com>
2017-09-16 23:59:46 +0300

    Basic recyclerView

--- app/src/main/java/mobileschool/tables/MainActivity.java
+++ app/src/main/java/mobileschool/tables/MainActivity.java
@@ -2,21 +2,29 @@ package mobileschool.tables;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+
+import java.util.Collections;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final int MAX_RESULTS = 70;
+
     @BindView(R.id.etSearch)
     EditText etSearch;
     @BindView(R.id.btnSearch)
     Button btnSearch;
-    @BindView(R.id.tvResult)
-    TextView tvResult;
+    @BindView(R.id.rv)
+    RecyclerView recyclerView;
+
+    SearchResultsAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +38,21 @@ public class MainActivity extends AppCompatActivity {
 
         btnSearch.setOnClickListener(view -> {
             String text = etSearch.getText().toString();
-            tvResult.setText(text);
+            List<String> results = Collections.nCopies(MAX_RESULTS, text);
+            showResultsInRecyclerView(results);
         });
+    }
+
+    private void showResultsInRecyclerView(List<String> results) {
+
+        if (adapter == null) {
+            adapter = new SearchResultsAdapter(results);
+            recyclerView.setAdapter(adapter);
+            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+            recyclerView.setLayoutManager(layoutManager);
+        } else {
+            adapter.setResults(results);
+            adapter.notifyDataSetChanged();
+        }
     }
 }
---
+++ app/src/main/java/mobileschool/tables/SearchResultsAdapter.java
@@ -0,0 +1,62 @@
+package mobileschool.tables;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by esaidoff on 16/09/2017.
+ */
+
+public class SearchResultsAdapter extends RecyclerView.Adapter<SearchResultsAdapter.ViewHolder> {
+
+    private List<String> results;
+
+    public SearchResultsAdapter(List<String> results) {
+        this.results = results;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View root = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_item, parent, false);
+        return new ViewHolder(root);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+        holder.bind(position);
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return results.size();
+    }
+
+    public void setResults(List<String> results) {
+        this.results = results;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tvResult;
+
+        public ViewHolder(View itemView) {
+
+            super(itemView);
+            tvResult = itemView.findViewById(R.id.tvSearchResult);
+        }
+
+        public void bind(int position) {
+
+            String result = results.get(position);
+            tvResult.setText(result + " " + position);
+        }
+    }
+}
